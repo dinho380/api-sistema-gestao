d596821b15a00bf36a1c72abe9d034bfc317791e
dinho380 <k2apcnccr@gmail.com>
2024-03-15 19:29:00 -0300

    Update users.js
    update cors

--- routers/users.js
+++ routers/users.js
@@ -2,9 +2,10 @@ const express = require('express');
 const {addUser,getUsers,updateUser} = require('../controllers/resCliente');
 const {addUserServicos,getUsersServicos,updateUserServicos} = require("../controllers/resServico");
 const { getUsersLogin } = require('../controllers/resLogin');
+const cors = require('cors');
 
 const router = express.Router();
-
+router.use(cors());
 //CLIENTES
 router.get('/',getUsers);
 router.post('/',addUser);
